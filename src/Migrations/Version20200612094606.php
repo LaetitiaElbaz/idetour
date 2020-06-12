d7dfe186a539cad28ba2b6ffcda5198337852be3
LaetitiaElbaz <laetitiaelbaz.web@gmail.com>
2020-06-12 11:46:51 +0200

    Add migration for City entity

---
+++ src/Migrations/Version20200612094606.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200612094606 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE city ADD city_gps_lat NUMERIC(10, 6) DEFAULT NULL, ADD city_gps_lng NUMERIC(10, 6) DEFAULT NULL, ADD city_slug VARCHAR(255) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE city DROP city_gps_lat, DROP city_gps_lng, DROP city_slug');
+    }
+}
