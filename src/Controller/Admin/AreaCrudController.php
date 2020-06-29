e6ea8fa06de69e5c6bf12b132d1e6b2d311ca65d
LaetitiaElbaz <laetitiaelbaz.web@gmail.com>
2020-06-29 11:29:02 +0200

    Configure AreaCrudController

--- src/Controller/Admin/AreaCrudController.php
+++ src/Controller/Admin/AreaCrudController.php
@@ -4,6 +4,10 @@ namespace App\Controller\Admin;
 
 use App\Entity\Area;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class AreaCrudController extends AbstractCrudController
 {
@@ -12,14 +16,43 @@ class AreaCrudController extends AbstractCrudController
         return Area::class;
     }
 
-    /*
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            // the labels used to refer to this entity in titles, buttons, etc.
+            ->setEntityLabelInSingular('région')
+            ->setEntityLabelInPlural('régions')
+
+            // the Symfony Security permission needed to manage the entity
+            // (none by default, so you can manage all instances of the entity)
+            // ->setEntityPermission('ROLE_EDITOR')
+
+            // the visible title at the top of the page and the content of the <title> element
+            // it can include these placeholders: %entity_id%, %entity_label_singular%, %entity_label_plural%
+            ->setPageTitle('index', 'Liste des %entity_label_plural% ')
+
+            // the names of the Doctrine entity properties where the search is made on
+            // (by default it looks for in all properties)
+            ->setSearchFields(['id', 'areaCode', 'areaName', 'createdAt', 'updatedAt'])
+
+            // defines the initial sorting applied to the list of entities
+            // (user can later change this sorting by clicking on the table columns)
+            ->setDefaultSort(['id' => 'DESC', 'areaName' => 'ASC'])
+
+            // the max number of entities to display per page
+            ->setPaginatorPageSize(10);
+    }
+
     public function configureFields(string $pageName): iterable
     {
+
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->hideOnForm(),
+            TextField::new('areaCode', 'Code pays'),
+            TextField::new('areaName', 'Nom du pays'),
+            DateTimeField::new('createdAt')->onlyOnDetail(),
+            DateTimeField::new('updatedAt')->onlyOnDetail(),
+            TextField::new('areaSlug')->onlyOnDetail(),
         ];
     }
-    */
 }
