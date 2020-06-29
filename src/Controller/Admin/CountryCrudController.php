0485f73a87cdda78c5da5b2928850a61c8438e4e
LaetitiaElbaz <laetitiaelbaz.web@gmail.com>
2020-06-29 11:24:43 +0200

    Configure CountryCrudController

--- src/Controller/Admin/CountryCrudController.php
+++ src/Controller/Admin/CountryCrudController.php
@@ -16,51 +16,46 @@ class CountryCrudController extends AbstractCrudController
         return Country::class;
     }
 
-    // public function configureCrud(Crud $crud): Crud
-    // {
-    //     return $crud
-    //         // the labels used to refer to this entity in titles, buttons, etc.
-    //         ->setEntityLabelInSingular('Pays')
-    //         ->setEntityLabelInPlural('Pays')
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            // the labels used to refer to this entity in titles, buttons, etc.
+            ->setEntityLabelInSingular('pays')
+            ->setEntityLabelInPlural('pays')
 
-    //         // the Symfony Security permission needed to manage the entity
-    //         // (none by default, so you can manage all instances of the entity)
-    //         // ->setEntityPermission('ROLE_EDITOR')
+            // the Symfony Security permission needed to manage the entity
+            // (none by default, so you can manage all instances of the entity)
+            // ->setEntityPermission('ROLE_EDITOR')
 
-    //         // the visible title at the top of the page and the content of the <title> element
-    //         // it can include these placeholders: %entity_id%, %entity_label_singular%, %entity_label_plural%
-    //         ->setPageTitle('index', '%entity_label_plural% listing')
+            // the visible title at the top of the page and the content of the <title> element
+            // it can include these placeholders: %entity_id%, %entity_label_singular%, %entity_label_plural%
+            ->setPageTitle('index', 'Liste des %entity_label_plural% ')
 
-    //         // the names of the Doctrine entity properties where the search is made on
-    //         // (by default it looks for in all properties)
-    //         ->setSearchFields(['id','countryCode', 'CountryName', 'createdAt','updatedAt'])
+            // the names of the Doctrine entity properties where the search is made on
+            // (by default it looks for in all properties)
+            ->setSearchFields(['id','countryCode', 'CountryName', 'createdAt','updatedAt'])
 
-    //         // defines the initial sorting applied to the list of entities
-    //         // (user can later change this sorting by clicking on the table columns)
-    //         ->setDefaultSort(['id' => 'DESC', 'countryName' => 'ASC'])
+            // defines the initial sorting applied to the list of entities
+            // (user can later change this sorting by clicking on the table columns)
+            ->setDefaultSort(['id' => 'DESC', 'countryName' => 'ASC'])
 
-    //         // the max number of entities to display per page
-    //         ->setPaginatorPageSize(10)
+            // the max number of entities to display per page
+            ->setPaginatorPageSize(10)
 
-    //         // this method allows to use your own template to render a certain part
-    //         // of the backend instead of using EasyAdmin default template
-    //         // the first argument is the "template name", which is the same as the
-    //         // Twig path but without the `@EasyAdmin/` prefix
-    //         ->overrideTemplate('crud/field/id', 'admin/fields/my_id.html.twig')
-    //     ;
-    // }
+        ;
+    }
     
-    // public function configureFields(string $pageName): iterable
-    // {
+    public function configureFields(string $pageName): iterable
+    {
 
-    //     return [
-    //         IdField::new('id')->hideOnForm(),
-    //         TextField::new('countryCode'),
-    //         TextField::new('countryName'),
-    //         DateTimeField::new('createdAt')->onlyOnDetail(),
-    //         DateTimeField::new('updatedAt')->onlyOnDetail(),
-    //         TextField::new('countrySlug'),
-    //     ];
-    // }
+        return [
+            IdField::new('id')->hideOnForm(),
+            TextField::new('countryCode', 'Code pays'),
+            TextField::new('countryName', 'Nom du pays'),
+            DateTimeField::new('createdAt')->onlyOnDetail(),
+            DateTimeField::new('updatedAt')->onlyOnDetail(),
+            TextField::new('countrySlug')->onlyOnDetail(),
+        ];
+    }
     
 }
