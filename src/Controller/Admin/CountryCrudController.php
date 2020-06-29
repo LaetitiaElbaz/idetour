e3fe59ba781839be002343911da503dc61149b66
LaetitiaElbaz <laetitiaelbaz.web@gmail.com>
2020-06-29 11:00:36 +0200

    Config dashboard + CRUD of each entity + some naming modification in entities

--- src/Controller/Admin/CountryCrudController.php
+++ src/Controller/Admin/CountryCrudController.php
@@ -4,6 +4,10 @@ namespace App\Controller\Admin;
 
 use App\Entity\Country;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class CountryCrudController extends AbstractCrudController
 {
@@ -12,14 +16,51 @@ class CountryCrudController extends AbstractCrudController
         return Country::class;
     }
 
-    /*
-    public function configureFields(string $pageName): iterable
-    {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
-    }
-    */
+    // public function configureCrud(Crud $crud): Crud
+    // {
+    //     return $crud
+    //         // the labels used to refer to this entity in titles, buttons, etc.
+    //         ->setEntityLabelInSingular('Pays')
+    //         ->setEntityLabelInPlural('Pays')
+
+    //         // the Symfony Security permission needed to manage the entity
+    //         // (none by default, so you can manage all instances of the entity)
+    //         // ->setEntityPermission('ROLE_EDITOR')
+
+    //         // the visible title at the top of the page and the content of the <title> element
+    //         // it can include these placeholders: %entity_id%, %entity_label_singular%, %entity_label_plural%
+    //         ->setPageTitle('index', '%entity_label_plural% listing')
+
+    //         // the names of the Doctrine entity properties where the search is made on
+    //         // (by default it looks for in all properties)
+    //         ->setSearchFields(['id','countryCode', 'CountryName', 'createdAt','updatedAt'])
+
+    //         // defines the initial sorting applied to the list of entities
+    //         // (user can later change this sorting by clicking on the table columns)
+    //         ->setDefaultSort(['id' => 'DESC', 'countryName' => 'ASC'])
+
+    //         // the max number of entities to display per page
+    //         ->setPaginatorPageSize(10)
+
+    //         // this method allows to use your own template to render a certain part
+    //         // of the backend instead of using EasyAdmin default template
+    //         // the first argument is the "template name", which is the same as the
+    //         // Twig path but without the `@EasyAdmin/` prefix
+    //         ->overrideTemplate('crud/field/id', 'admin/fields/my_id.html.twig')
+    //     ;
+    // }
+    
+    // public function configureFields(string $pageName): iterable
+    // {
+
+    //     return [
+    //         IdField::new('id')->hideOnForm(),
+    //         TextField::new('countryCode'),
+    //         TextField::new('countryName'),
+    //         DateTimeField::new('createdAt')->onlyOnDetail(),
+    //         DateTimeField::new('updatedAt')->onlyOnDetail(),
+    //         TextField::new('countrySlug'),
+    //     ];
+    // }
+    
 }
--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -21,9 +21,11 @@ use App\Entity\Tag;
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Config\UserMenu;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class DashboardController extends AbstractDashboardController
 {
@@ -45,22 +47,26 @@ class DashboardController extends AbstractDashboardController
     {
         return [
             MenuItem::linkToDashboard('Dashboard', 'fa fa-home'),
+            // MenuItem::linkToLogout('Logout', 'fa fa-sign-out'),
+
+            MenuItem::section('Centres d\'intérêt'),
+            MenuItem::linkToCrud('Centres d\'intérêt', 'fa fa-map-marker', Poi::class)
+            ->setController(Offer::class)
+                ->setController(PriceCrudController::class)
+                ->setController(ContactCrudController::class)
+                ->setController(RoleCrudController::class)
+                ->setQueryParameter('sortField', 'poiName'),
+
 
             MenuItem::section('Localisation'),
             MenuItem::linkToCrud('Pays', 'fa fa-globe', Country::class),
+                // ->setAction('new'),
             MenuItem::linkToCrud('Régions', 'fa fa-globe', Area::class),
             MenuItem::linkToCrud('Départements', 'fa fa-globe', Department::class),
             MenuItem::linkToCrud('Villes', 'fa fa-globe', City::class),
 
-            MenuItem::section('Centres d\'intérêt'),
-            MenuItem::linkToCrud('Centres d\'intérêt', 'fa fa-map-marker', Poi::class),
-            MenuItem::linkToCrud('Contacts', 'fa fa-address-book', Contact::class),
-            MenuItem::linkToCrud('Rôles', 'fa fa-address-book', Role::class),
-
-            MenuItem::section('Offres'),
-            MenuItem::linkToCrud('Offres', 'fa fa-map-marker', Offer::class),
+            MenuItem::section('Public'),
             MenuItem::linkToCrud('Public', 'fa fa-users', Audience::class),
-            MenuItem::linkToCrud('Prix', 'fa fa-money', Price::class),
 
             MenuItem::section('Catégories'),
             MenuItem::linkToCrud('Catégories', 'fa fa-tags', Category::class),
@@ -70,6 +76,7 @@ class DashboardController extends AbstractDashboardController
             MenuItem::section('Utilisateurs'),
             MenuItem::linkToCrud('Utilisateurs', 'fa fa-users', User::class),
             MenuItem::linkToCrud('Commentaires', 'fa fa-comment', Comment::class),
+            MenuItem::linkToExitImpersonation('Arrêtez l\'emprunt d\'identité', 'fa fa-sign-out'),
 
             MenuItem::section('Périodes'),
             MenuItem::linkToCrud('Périodes', 'fa fa-calendar', Period::class),
@@ -77,4 +84,27 @@ class DashboardController extends AbstractDashboardController
         ];
 
     }
+
+    public function configureUserMenu(UserInterface $user): UserMenu
+    {
+        // Usually it's better to call the parent method because that gives you a
+        // user menu with some menu items already created ("sign out", "exit impersonation", etc.)
+        // if you prefer to create the user menu from scratch, use: return UserMenu::new()->...
+        return parent::configureUserMenu($user)
+            // use the given $user object to get the user name
+            ->setName($user->getUsername())
+            // use this method if you don't want to display the user image
+            ->displayUserAvatar(false)
+            // you can also pass an email address to use gravatar's service
+            // ->setGravatarEmail($user->getEmail())
+
+            // you can use any type of menu item
+            ->addMenuItems([
+                MenuItem::linkToRoute('Mon profile', 'fa fa-id-card', '...', ['...' => '...']),
+                MenuItem::linkToRoute('Settings', 'fa fa-user-cog', '...', ['...' => '...']),
+                MenuItem::section(),
+                MenuItem::linkToLogout('Déconnexion', 'fa fa-sign-out'),
+            ]);
+    }
+
 }
